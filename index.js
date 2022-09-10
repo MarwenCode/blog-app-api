4d1051ecdc13eb06fe7b13ba3cfd16d726cad44f
MarwenCode <marwen_jdidi@yahoo.com>
2022-09-10 11:04:41 +0200

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import postRoute from "./routes/posts.js";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-require("dotenv").config()
+
+
+// require("dotenv").config()
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -49,7 +51,11 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
 
-app.listen(process.env.PORT || 8000, () => {
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
   connect();
   console.log("connected to backend");
-});
+}); 
+
+
