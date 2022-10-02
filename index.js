73e81aab397bf981ba315d678666039b983a14ca
MarwenCode <marwen_jdidi@yahoo.com>
2022-10-02 23:11:06 +0200

    add cors site

--- index.js
+++ index.js
@@ -51,13 +51,13 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
-app.use(cors({
-  origin:"https://blog-app-frontend.onrender.com"
-}))
+// app.use(cors({
+//   origin:"https://blog-app-frontend.onrender.com"
+// }))
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://blog-app-frontend.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
