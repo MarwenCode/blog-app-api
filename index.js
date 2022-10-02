b245a4f092cbe7fbd492f5a3e4b07dcb069e8ddd
MarwenCode <marwen_jdidi@yahoo.com>
2022-10-02 22:55:26 +0200

    cors

--- index.js
+++ index.js
@@ -55,6 +55,16 @@ app.use(cors({
   origin:"https://blog-app-frontend.onrender.com"
 }))
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('*', (req, res) => {
